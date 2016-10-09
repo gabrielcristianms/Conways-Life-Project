fcf38091fa6f2524643870358e554368e2e2eab5
Gabriel Cristian <gabrielcristianms@gmail.com>
2016-10-09 15:34:24 -0300

    Finalizada assinatura da classe Life

--- include/life.h
+++ include/life.h
@@ -7,7 +7,20 @@
 
 class Life{
 	public:
-		
+		using size_type = unsigned long;
+
+		Life( std::ostream );
+		Life( const Life& )
+		~Life();
+		void update();
+		void print() const;
+		bool operator==() const;
+		bool operator!=() const;
+		Life& operator=( const Life& );
+		friend std::ostream& operator<<( std::ostream, Life& );
 	private:
-		
+		bool **m_cells;
+		size_type m_height;
+		size_type m_width;
 };
+#endif
