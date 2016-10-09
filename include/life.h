6a73126672db8839cc5301d1b6c8d585804540a3
Gabriel Cristian <gabrielcristianms@gmail.com>
2016-10-09 19:07:03 -0300

    Comentários e finalização das assinaturas

--- include/life.h
+++ include/life.h
@@ -2,22 +2,32 @@
 #define __LIFE_H__
 #include <iostream>
 #include <cassert>
-#include <algorithms>
+#include <algorithm>
 #include <cstdlib>
 
 class Life{
 	public:
+		//Defining a type for the size of the matrix
 		using size_type = unsigned long;
 
-		Life( std::ostream );
-		Life( const Life& )
+		//Default Constructor
+		Life( size_type, size_type, std::ostream& );
+		//Copy Constructor
+		Life( const Life& );
+		//Destructor
 		~Life();
-		void update();
-		void print() const;
-		bool operator==() const;
-		bool operator!=() const;
+		//This function aplies the rules of Conway's Game of Life, killing or creating cells.
+		void update( void );
+		//This function prints the actual configuration in the screen.
+		void print( void ) const;
+		//Overloading the == operator to compare different Life instances.
+		bool operator==( Life& ) const;
+		//Overloading the != operator to compare different Life instances.
+		bool operator!=( Life& ) const;
+		//Copy assignment.
 		Life& operator=( const Life& );
-		friend std::ostream& operator<<( std::ostream, Life& );
+		//Overloading the << operator to print Life objects using cout and similar functions.
+		friend std::ostream& operator<<( std::ostream&, Life& );
 	private:
 		bool **m_cells;
 		size_type m_height;
